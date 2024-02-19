17c641ee5a537284eae9ddf931186a51ed8b0186
GustavoNoronha0 <admin@gustavonoronha.com.br>
2024-02-19 20:24:12 -0300

    feat: add new logo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,14 +9,13 @@ import 'react-toastify/dist/ReactToastify.css'
 import GlobalStyles from '@/styles/global'
 import theme from '@/styles/theme'
 
-
 function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Head>
         <title>Gustavo Noronha</title>
-        <link rel="shortcut icon" href="/img/logo.png" />
-        <link rel="apple-touch-icon" href="/img/logo.png" />
+        <link rel="shortcut icon" href="/img/gustavo.jpg" />
+        <link rel="apple-touch-icon" href="/img/gustavo.jpg" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="description" content="Software Engineer" />
       </Head>
