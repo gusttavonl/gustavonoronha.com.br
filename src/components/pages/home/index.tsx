7fbbc6607fc1bbbdeef6aa394fee72bc8fd14360
GustavoNoronha0 <admin@gustavonoronha.com.br>
2024-02-19 09:51:42 -0300

    feat: add personal infos on home

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -3,7 +3,20 @@ import * as S from './styles'
 const HomeComponent = () => {
   return (
     <S.Container>
-      <h1>home</h1>
+      <S.Title>Gustavo Noronha</S.Title>
+      <S.SubTitle>Software Engineer & Content Creator</S.SubTitle>
+      <S.About>
+        {`
+        Hello! I'm Gustavo Noronha, a software engineer with a passion for technology. With six years of experience, I've worked across diverse industries, specializing in areas such as payments, management software, streaming, and credit.
+
+        My expertise extends to both front-end and back-end development, with proficiency in languages like JavaScript, Rust, Go, and Python, as well as technologies such as React. I have a strong background in building microservices architectures and implementing messaging systems.
+
+        While serving as the Chief Technology Officer (CTO) of my own company, I've led the successful development of our Learning Management System (LMS) from inception to deployment. Additionally, I currently hold the position of tech lead at another company, where I oversee project development, mentor team members, and tackle complex challenges.
+
+        Beyond my professional endeavors, I'm also a content creator on YouTube, where I share insights on technology and personal development.
+        `}
+      </S.About>
+      <S.Contact>Contact</S.Contact>
     </S.Container>
   )
 }
--- src/components/pages/home/styles.ts
+++ src/components/pages/home/styles.ts
@@ -1,7 +1,48 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  max-width: 95%;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
+
+  padding: 50px;
+`
+
+export const Title = styled.h1`
+  width: 100%;
+  margin-top: 50px;
+  ${({ theme }) => css`
+    font-family: ${theme.font.family};
+    font-size: ${theme.font.sizes.large};
+    font-weight: ${theme.font.bold};
+    color: ${theme.colors.gray300};
+  `}
+`
+
+export const SubTitle = styled.h3`
+  width: 100%;
+  margin-top: 2px;
+  ${({ theme }) => css`
+    font-family: ${theme.font.family};
+    font-size: ${theme.font.sizes.medium};
+    font-weight: ${theme.font.bold};
+    color: ${theme.colors.gray400};
+  `}
+`
+
+export const About = styled.p`
+  width: 100%;
+  margin-top: 50px;
+  ${({ theme }) => css`
+    font-family: ${theme.font.family};
+    font-size: ${theme.font.sizes.medium};
+    font-weight: ${theme.font.bold};
+    color: ${theme.colors.gray400};
+  `}
+`
+
+export const Contact = styled.a`
+  width: 100%;
 `
